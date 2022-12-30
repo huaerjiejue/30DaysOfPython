7b2589562c4b6f1c7912331d8e878eaf22b863ef
huaerjiejue <emtric.cn@gmail.com>
2022-12-30 18:18:29 +0800

    奥特曼打小怪兽

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-print('I hope everyone is enjoying the Python Challenge.\nAre you ?') # line break
-print('Days\tTopics\tExercises') # adding tab space or 4 spaces
+print('I hope everyone is enjoying the Python Challenge.\nAre you ?')  # line break
+print('Days\tTopics\tExercises')  # adding tab space or 4 spaces
 print('Day 1\t3\t5')
 print('Day 2\t3\t5')
 print('Day 3\t3\t5')
 print('Day 4\t3\t5')
-print('This is a backslash  symbol (\\)') # To write a backslash
-print('In every programming language it starts with \"Hello, World!\"') # to write a double quote inside a single quote
+print('This is a backslash  symbol (\\)')  # To write a backslash
+print('In every programming language it starts with \"Hello, World!\"')  # to write a double quote inside a single quote
 
 first_name = 'John'
 last_name = 'Smith'
 message = first_name + ' [' + first_name[0] + '] is a coder'
 lalala = '%s [%s] is a coder' % (first_name, first_name[0])
 first_name = 'Asabeneh'
-last_name = 'Yetayeh'
+# last_name = 'Yetayeh'
 language = 'Python'
 formated_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
 print(formated_string)
@@ -22,14 +22,14 @@ b = 3
 print('{} + {} = {}'.format(a, b, a + b))
 print('{} - {} = {}'.format(a, b, a - b))
 print('{} * {} = {}'.format(a, b, a * b))
-print('{} / {} = {:.2f}'.format(a, b, a / b)) # limits it to two digits after decimal
+print('{} / {} = {:.2f}'.format(a, b, a / b))  # limits it to two digits after decimal
 print('{} % {} = {}'.format(a, b, a % b))
 print('{} // {} = {}'.format(a, b, a // b))
 print('{} ** {} = {}'.format(a, b, a ** b))
 
 a = 4
 b = 3
-print(f'{a} + {b} = {a +b}')
+print(f'{a} + {b} = {a + b}')
 print(f'{a} - {b} = {a - b}')
 print(f'{a} * {b} = {a * b}')
 print(f'{a} / {b} = {a / b:.2f}')
@@ -53,86 +53,311 @@ print(f'{a} ** {b} = {a ** b}')
 
 # list slicing
 language = 'Python'
-first_three = language[0:3] # starts at zero index and up to 3 but not include 3
-print(first_three) #Pyt
+first_three = language[0:3]  # starts at zero index and up to 3 but not include 3
+print(first_three)  # Pyt
 last_three = language[3:6]  # 3,4,5
-print(last_three) # hon
+print(last_three)  # hon
 # Another way
 last_three = language[-3:]
-print(last_three)   # hon
+print(last_three)  # hon
 last_three = language[3:]
-print(last_three)   # hon
+print(last_three)  # hon
 first_three = language[:-3]
 
 greeting = 'Hello, World!'
-print(greeting[::-1]) # !dlroW ,olleH , reverse the string
+print(greeting[::-1])  # !dlroW ,olleH , reverse the string
 
 language = 'Python'
-pto = language[0:6:2] #
-print(pto) # Pto
+pto = language[0:6:2]  #
+print(pto)  # Pto
 
 # capitalize(): Converts (转换) the first character of the string (字符串) to capital letter
 my_test_string = 'python is easy to learn'
-print(my_test_string.capitalize()) # Python is easy to learn
+print(my_test_string.capitalize())  # Python is easy to learn
 
 # casefold(): Converts string into lower case
 my_test_string = 'PYTHON IS EASY TO LEARN'
-print(my_test_string.casefold()) # python is easy to learn
+print(my_test_string.casefold())  # python is easy to learn
 
-#count(): Returns the number of times a specified value occurs in a string
+# count(): Returns the number of times a specified value occurs in a string
 challenge = 'thirty days of python'
-print(challenge.count('y')) # 3
-print(challenge.count('y', 7, 14)) # 1,
-print(challenge.count('th')) # 2`
+print(challenge.count('y'))  # 3
+print(challenge.count('y', 7, 14))  # 1,
+print(challenge.count('th'))  # 2`
 
-#endswith(): Returns true if the string ends with the specified value
+# endswith(): Returns true if the string ends with the specified value
 challenge = 'thirty days of python'
-print(challenge.endswith('on'))   # True
-print(challenge.endswith('tion')) # False
+print(challenge.endswith('on'))  # True
+print(challenge.endswith('tion'))  # False
 
-#expandtabs(): Sets the tab size of the string
+# expandtabs(): Sets the tab size of the string
 challenge = 'thirty\tdays\tof\tpython'
-print(challenge.expandtabs())   # 'thirty  days    of      python'
-print(challenge.expandtabs(10)) # 'thirty    days      of        python'
+print(challenge.expandtabs())  # 'thirty  days    of      python'
+print(challenge.expandtabs(10))  # 'thirty    days      of        python'
 print(challenge.expandtabs(8))  # 'thirty  days    of      python'
 
-#find(): Searches the string for a specified value and returns the position of where it was found
-#find()方法和index()方法的区别在于，如果找不到，find()返回-1，而index()会报错,if not found, find() returns -1, while index() raises an exception
+# find(): Searches the string for a specified value and returns the position of where it was found
+# find()方法和index()方法的区别在于，如果找不到，find()返回-1，而index()会报错,
+# if not found, find() returns -1, while index() raises an exception
 challenge = 'thirty days of python'
 print(challenge.find('y'))  # 5
-print(challenge.find('th')) # 0
+print(challenge.find('th'))  # 0
 
-#rfind(): Searches the string for a specified value and returns the last position of where it was found
-#find return the first position of where it was found, rfind return the last position of where it was found
+# rfind(): Searches the string for a specified value and returns the last position of where it was found
+# find return the first position of where it was found, rfind return the last position of where it was found
 challenge = 'thirty days of python'
 print(challenge.rfind('y'))  # 16
-print(challenge.rfind('th')) # 17
+print(challenge.rfind('th'))  # 17
 
-#isalum(): Returns True if all characters in the string are alphanumeric
+# isalum(): Returns True if all characters in the string are alphanumeric
 challenge = 'ThirtyDaysPython'
-print(challenge.isalnum()) # True
+print(challenge.isalnum())  # True
 
 challenge = '30DaysPython'
-print(challenge.isalnum()) # True
+print(challenge.isalnum())  # True
 
 challenge = 'thirty days of python'
-print(challenge.isalnum()) # False, space is not an alphanumeric character
+print(challenge.isalnum())  # False, space is not an alphanumeric character
 
 challenge = 'thirty days of python 2019'
-print(challenge.isalnum()) # False, space is not an alphanumeric character
+print(challenge.isalnum())  # False, space is not an alphanumeric character
 
-#join(): Joins the elements of an iterable to the end of the string
+# join(): Joins the elements of an iterable to the end of the string
 web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
 result = ' '.join(web_tech)
-print(result) # 'HTML CSS JavaScript React'
+print(result)  # 'HTML CSS JavaScript React'
 
-#strip(): Returns a trimmed version of the string
+# strip(): Returns a trimmed version of the string
 challenge = 'thirty days of pythoonnn'
-print(challenge.strip('noth')) # 'irty days of py'
+print(challenge.strip('noth'))  # 'irty days of py'
 
-#split(): Splits the string at the specified separator, and returns a list
+# split(): Splits the string at the specified separator, and returns a list
 challenge = 'thirty days of python'
-print(challenge.split()) # ['thirty', 'days', 'of', 'python']
+print(challenge.split())  # ['thirty', 'days', 'of', 'python']
 challenge = 'thirty, days, of, python'
-print(challenge.split(', ')) # ['thirty', 'days', 'of', 'python']
-print(type(challenge.split(', '))) # <class 'list'>
+print(challenge.split(', '))  # ['thirty', 'days', 'of', 'python']
+print(type(challenge.split(', ')))  # <class 'list'>
+
+from time import sleep
+
+
+class Clock(object):
+    """数字时钟"""
+
+    def __init__(self, hour=0, minute=0, second=0):
+        """初始化方法
+
+        :param hour: 时
+        :param minute: 分
+        :param second: 秒
+        """
+        self._hour = hour
+        self._minute = minute
+        self._second = second
+
+    def run(self):
+        """走字"""
+        self._second += 1
+        if self._second == 60:
+            self._second = 0
+            self._minute += 1
+            if self._minute == 60:
+                self._minute = 0
+                self._hour += 1
+                if self._hour == 24:
+                    self._hour = 0
+
+    def show(self):
+        """显示时间"""
+        return '%02d:%02d:%02d' % \
+            (self._hour, self._minute, self._second)
+
+
+from abc import ABCMeta, abstractmethod
+from random import randint, randrange
+
+
+class Fighter(object, metaclass=ABCMeta):
+    """战斗者"""
+
+    # 通过__slots__魔法限定对象可以绑定的成员变量
+    __slots__ = ('_name', '_hp')
+
+    def __init__(self, name, hp):
+        """初始化方法
+
+        :param name: 名字
+        :param hp: 生命值
+        """
+        self._name = name
+        self._hp = hp
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, hp):
+        self._hp = hp if hp >= 0 else 0
+
+    @property
+    def alive(self):
+        return self._hp > 0
+
+    @abstractmethod
+    def attack(self, other):
+        """攻击
+
+        :param other: 被攻击的对象
+        """
+        pass
+
+
+class Ultraman(Fighter):
+    """奥特曼"""
+
+    __slots__ = ('_name', '_hp', '_mp')
+
+    def __init__(self, name, hp, mp):
+        """初始化方法
+
+        :param name: 名字
+        :param hp: 生命值
+        :param mp: 魔法值
+        """
+        super().__init__(name, hp)
+        self._mp = mp
+
+    def attack(self, other):
+        other.hp -= randint(15, 25)
+
+    def huge_attack(self, other):
+        """究极必杀技(打掉对方至少50点或四分之三的血)
+
+        :param other: 被攻击的对象
+
+        :return: 使用成功返回True否则返回False
+        """
+        if self._mp >= 50:
+            self._mp -= 50
+            injury = other.hp * 3 // 4
+            injury = injury if injury >= 50 else 50
+            other.hp -= injury
+            return True
+        else:
+            self.attack(other)
+            return False
+
+    def magic_attack(self, others):
+        """魔法攻击
+
+        :param others: 被攻击的群体
+
+        :return: 使用魔法成功返回True否则返回False
+        """
+        if self._mp >= 20:
+            self._mp -= 20
+            for temp in others:
+                if temp.alive:
+                    temp.hp -= randint(10, 15)
+            return True
+        else:
+            return False
+
+    def resume(self):
+        """恢复魔法值"""
+        incr_point = randint(1, 10)
+        self._mp += incr_point
+        return incr_point
+
+    def __str__(self):
+        return '~~~%s奥特曼~~~\n' % self._name + \
+            '生命值: %d\n' % self._hp + \
+            '魔法值: %d\n' % self._mp
+
+
+class Monster(Fighter):
+    """小怪兽"""
+
+    __slots__ = ('_name', '_hp')
+
+    def attack(self, other):
+        other.hp -= randint(10, 20)
+
+    def __str__(self):
+        return '~~~%s小怪兽~~~\n' % self._name + \
+            '生命值: %d\n' % self._hp
+
+
+def is_any_alive(monsters):
+    """判断有没有小怪兽是活着的"""
+    for monster in monsters:
+        if monster.alive > 0:
+            return True
+    return False
+
+
+def select_alive_one(monsters):
+    """选中一只活着的小怪兽"""
+    monsters_len = len(monsters)
+    while True:
+        index = randrange(monsters_len)
+        monster = monsters[index]
+        if monster.alive > 0:
+            return monster
+
+
+def display_info(ultraman, monsters):
+    """显示奥特曼和小怪兽的信息"""
+    print(ultraman)
+    for monster in monsters:
+        print(monster, end='')
+
+
+def main():
+    u = Ultraman('骆昊', 1000, 120)
+    m1 = Monster('狄仁杰', 250)
+    m2 = Monster('白元芳', 500)
+    m3 = Monster('王大锤', 750)
+    ms = [m1, m2, m3]
+    fight_round = 1
+    print('---战斗开始---')
+    print('---对战双方信息---')
+    display_info(u, ms)
+    while u.alive and is_any_alive(ms):
+        print('========第%02d回合========' % fight_round)
+        m = select_alive_one(ms)  # 选中一只小怪兽
+        skill = randint(1, 10)  # 通过随机数选择使用哪种技能
+        if skill <= 6:  # 60%的概率使用普通攻击
+            print('%s使用普通攻击打了%s.' % (u.name, m.name))
+            u.attack(m)
+            print('%s的魔法值恢复了%d点.' % (u.name, u.resume()))
+        elif skill <= 9:  # 30%的概率使用魔法攻击(可能因魔法值不足而失败)
+            if u.magic_attack(ms):
+                print('%s使用了魔法攻击.' % u.name)
+            else:
+                print('%s使用魔法失败.' % u.name)
+        else:  # 10%的概率使用究极必杀技(如果魔法值不足则使用普通攻击)
+            if u.huge_attack(m):
+                print('%s使用究极必杀技虐了%s.' % (u.name, m.name))
+            else:
+                print('%s使用普通攻击打了%s.' % (u.name, m.name))
+                print('%s的魔法值恢复了%d点.' % (u.name, u.resume()))
+        if m.alive > 0:  # 如果选中的小怪兽没有死就回击奥特曼
+            print('%s回击了%s.' % (m.name, u.name))
+            m.attack(u)
+        display_info(u, ms)  # 每个回合结束后显示奥特曼和小怪兽的信息
+        fight_round += 1
+    print('\n========战斗结束!========\n')
+    if u.alive > 0:
+        print('%s奥特曼胜利!' % u.name)
+    else:
+        print('小怪兽胜利!')
+
+
+if __name__ == '__main__':
+    main()
